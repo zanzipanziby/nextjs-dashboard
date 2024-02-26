190aebc27db7da03f0d76e773271ded055a5762f
ZanziPanziBY <zanzipanziby@gmail.com>
2024-02-26 19:14:55 +0300

    feat: chapter-16 Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,7 +9,12 @@ import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
 // feat: chapter-10 Partial Prerendering (Optional)
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default async function Page() {
   return (
     <main>
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
 type Props = {
   searchParams?: {
@@ -14,6 +15,9 @@ type Props = {
   };
 };
 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page({ searchParams }: Props) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
